33ace01f0ad309a7a65d0f705ba5f54f22ad08f4
Murylo Saladino <murylosaladino@gmail.com>
2023-04-11 09:54:20 -0300

    inicial data

---
+++ src/scripts/data.js
@@ -0,0 +1,75 @@
+export let pokedex = [
+    {
+        id: '001',
+        name: 'bulbasaur',
+        types: ['grass', 'poison'],
+        family: 0,
+        img: '../src/assets/001.png',
+        color: '#8bc6ac'
+    },
+    {
+        id: '002',
+        name: 'ivysaur',
+        types: ['grass', 'poison'],
+        family: 0,
+        img: '../src/assets/002.png',
+        color: '#88bcbf'
+    },
+    {
+        id: '003',
+        name: 'venusaur',
+        types: ['grass', 'poison'],
+        family: 0,
+        img: '../src/assets/003.png',
+        color: '#68aaa7'
+    },
+    {
+        id: '004',
+        name: 'charmander',
+        types: ['fire'],
+        family: 1,
+        img: '../src/assets/004.png',
+        color: '#eaab7d'
+    },
+    {
+        id: '005',
+        name: 'charmeleon',
+        types: ['fire'],
+        family: 1,
+        img: '../src/assets/005.png',
+        color: '#e27f6e'
+    },
+    {
+        id: '006',
+        name: 'charizard',
+        types: ['fire', 'flying'],
+        family: 1,
+        img: '../src/assets/006.png',
+        color: '#eea365'
+    },
+    {
+        id: '007',
+        name: 'squirtle',
+        types: ['water'],
+        family: 2,
+        img: '../src/assets/007.png',
+        color: '#8fc4cf'
+    },
+    {
+        id: '008',
+        name: 'wartortle',
+        types: ['water'],
+        family: 2,
+        img: '../src/assets/008.png',
+        color: '#afbddc'
+    },
+    {
+        id: '009',
+        name: 'blastoise',
+        types: ['water'],
+        family: 2,
+        img: '../src/assets/009.png',
+        color: '#6e8cb4'
+    },
+    
+]
