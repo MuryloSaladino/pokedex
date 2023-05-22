854edf7ee0d057adf7dac4d5474037e78a753365
Murylo Saladino <murylosaladino@gmail.com>
2023-05-22 19:47:33 -0300

    develop branch

--- src/scripts/request.js
+++
@@ -1,8 +0,0 @@
-const defaultURL = 'https://pokeapi.co/api/v2/pokemon'
-
-export async function getThisPokemon(pokemon)   {
-    const info = await fetch(`${defaultURL}/${pokemon}`)
-    .then(response => response.json())
-
-    return info
-}
