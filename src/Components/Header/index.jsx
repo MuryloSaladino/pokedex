7d115de814eb46325d225c40c0ba0cdbdbe92afb
Murylo Saladino <murylosaladino@gmail.com>
2023-05-25 00:59:20 -0300

    update 25/05

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.css'
+import pokeball from '../../assets/pokeball.png'
+import { SearchInput } from '../SearchInput'
+
+export function Header() {
+    return(
+        <header>
+            <div>
+                
+            </div>
+            <SearchInput/>
+        </header>
+    )
+}
--- src/Components/PokeCard/PokeCard.jsx
+++
@@ -1,2 +0,0 @@
-import styles from './PokeCard.module.css'
-
