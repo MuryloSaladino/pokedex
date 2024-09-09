f6c9f94fbc96f65a7171a28a56145183ec20c6ac
MuryloSaladino <murylosaladino@gmail.com>
2024-09-09 10:32:43 -0300

    pokemon link

--- src/pages/Home/components/PokeCard.tsx
+++ src/pages/Home/components/PokeCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 interface IPokeCardProps {
     id: string | number;
@@ -8,8 +9,10 @@ interface IPokeCardProps {
 export default function PokeCard({ id, name }:IPokeCardProps) {
 
     return(
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <Typography variant="h4">#{ id } { name }</Typography>
-        </Box>
+        <Link to={"/p/" + id}>
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+                <Typography variant="h4">#{ id } { name }</Typography>
+            </Box>
+        </Link>
     )
 }
