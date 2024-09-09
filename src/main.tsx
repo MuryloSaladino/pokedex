c2fc29a55d2deada4db11cbe18b8eade8f131183
MuryloSaladino <murylosaladino@gmail.com>
2024-09-09 10:00:48 -0300

    homepage structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import "./styles/global.css"
+import MainRoutes from './routes/MainRoutes'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-  </StrictMode>,
+    <StrictMode>
+        <MainRoutes/>
+    </StrictMode>,
 )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,28 @@
-import { Box } from "@mui/material";
-import { ReactNode } from "react";
+import { Box, Typography } from "@mui/material";
+import { ReactNode, useEffect, useState } from "react";
+import PokeAPI from "../../service/PokeAPI";
+import { v4 as uuid } from "uuid";
 
 export default function Home():ReactNode {
 
+    const [pokemons, setPokemons] = useState<any[]>([])
+
+    useEffect(() => {
+        ( async () => {
+            const response = await PokeAPI.get('?limit=151&offset=0')
+            console.log(response)
+            setPokemons(response.data.results)
+        })()
+    }, [])
+
     return(
-        <Box>Home</Box>
+        <Box>
+            <Typography variant="h1">Home</Typography>
+            <ul>
+                {pokemons.map((pokemon, i) => (
+                    <li key={uuid()}>#{i+1}: {pokemon.name}</li>
+                ))}
+            </ul>
+        </Box>
     )
 }
