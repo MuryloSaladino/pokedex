805f137a571dc058ef11ba8e2f4bf1db8960217a
MuryloSaladino <murylosaladino@gmail.com>
2024-09-10 15:30:15 -0300

    theme pallete

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import "./styles/reset.css"
 import "./styles/global.css"
+
 import MainRoutes from './routes/MainRoutes'
+import { ThemeProvider } from '@mui/material'
+import theme from './styles/theme'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <MainRoutes/>
+        <ThemeProvider theme={theme}>
+            <MainRoutes/>
+        </ThemeProvider>
     </StrictMode>,
 )
