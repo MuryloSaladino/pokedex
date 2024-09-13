37f4459998301995febec55749fdb99006a7d9cd
MuryloSaladino <murylosaladino@gmail.com>
2024-09-13 10:04:35 -0300

    responsive title

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,5 +4,8 @@ export const SMainTitle = styled(Typography)(({ theme }) => ({
     color: theme.palette.primary.main,
     fontFamily: "Pokemon Solid",
     textAlign: "center",
-    WebkitTextStroke: "3px black"
+    WebkitTextStroke: "3px black",
+    [theme.breakpoints.down("sm")]: {
+        fontSize: "3.5rem"
+    }
 }))
